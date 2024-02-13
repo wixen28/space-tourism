51b80085ec5e07f852c7fc91bde87bc3a3d4cd05
Viktoria <115079673+wixen28@users.noreply.github.com>
2024-02-13 14:32:06 +0100

    Add files via upload

---
+++ src/technologyData.js
@@ -0,0 +1,39 @@
+import image1 from "./design/technology/image-launch-vehicle-portrait.jpg"
+import image2 from "./design/technology/image-launch-vehicle-landscape.jpg"
+import image3 from "./design/technology/image-spaceport-portrait.jpg"
+import image4 from "./design/technology/image-spaceport-landscape.jpg"
+import image5 from "./design/technology/image-space-capsule-portrait.jpg"
+import image6 from "./design/technology/image-space-capsule-landscape.jpg"
+
+
+const technologyData = [
+    {
+      "id": 0,
+      "name": "Launch vehicle",
+      "images": {
+        "portrait": image1,
+        "landscape": image2
+      },
+      "description": "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!"
+    },
+    {
+      "id": 1,
+      "name": "Spaceport",
+      "images": {
+        "portrait": image3,
+        "landscape": image4
+      },
+      "description": "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch."
+    },
+    {
+      "id": 2,
+      "name": "Space capsule",
+      "images": {
+        "portrait": image5,
+        "landscape": image6
+      },
+      "description": "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained."
+    }
+  ]
+
+  export default technologyData
